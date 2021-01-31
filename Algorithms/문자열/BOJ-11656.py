c0929e6a69d24bfc31f11185b00298fa3630edc2
GyuriLee <jae04099@gmail.com>
2021-01-31 23:17:53 +0900

    10828까지

--- Algorithms/문자열/BOJ-11656.py
+++ Algorithms/문자열/BOJ-11656.py
@@ -1,2 +1,20 @@
+# s = input()
+# word = ''
+# lists = []
+# for i in range(len(s)):
+#     word = s[i:len(s)]
+#     lists.append(word)
+# for i in range(len(lists)):
+#     lists = sorted(lists, key=lambda x : (x[i]))
+#     print(lists)
+# for i in range(len(lists)):
+#     print(lists[i])
+
 s = input()
-lists = 
+lists = []
+
+for _ in s:
+    lists.append(s)
+    s = s[1:]
+for i in sorted(lists):
+    print(i)
