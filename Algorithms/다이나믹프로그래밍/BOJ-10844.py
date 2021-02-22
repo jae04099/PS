cbb3e617c204c739d69fd2c9d1ded934471587e1
GyuriLee <jae04099@gmail.com>
2021-02-22 23:59:29 +0900

    .

--- Algorithms/다이나믹프로그래밍/BOJ-10844.py
+++ Algorithms/다이나믹프로그래밍/BOJ-10844.py
@@ -1,21 +1,56 @@
+# n = int(input())
+# d = [0] * (10 ** n)
+
+
+# def stair(x):
+#     x = str(x)
+#     for i in range(1, len(x)):
+#         if abs(int(x[i - 1]) - int(x[i])) == 1:
+#             return True
+
+
+# if n == 1:
+#     print(9)
+# else:
+#     for i in range(10 ** (n - 1), 10 ** n):
+#         if stair(i):
+#             d[i] = d[i - 1] + 1
+#         else:
+#             d[i] = d[i - 1]
+
+#     print(d[(10 ** n) - 1] % 1000000000)
+
+
+# N = int(input()) 
+# dp = [[0]*10 for _ in range(N+1)] 
+# for i in range(1, 10): 
+#     dp[1][i] = 1 
+    
+# MOD = 1000000000 
+    
+# for i in range(2, N+1): 
+#     for j in range(10): 
+#         if j == 0: 
+#             dp[i][j] = dp[i-1][1] 
+#         elif j == 9: 
+#             dp[i][j] = dp[i-1][8] 
+#         else: 
+#             dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
+# print(sum(dp[N]) % MOD)
+
+
 n = int(input())
-d = [0] * (10 ** n)
+d = [list([0] * 10) for _ in range(n + 1)]
 
+for i in range(1, 10):
+    d[1][i] = 1
 
-def stair(x):
-    x = str(x)
-    for i in range(1, len(x)):
-        if abs(int(x[i - 1]) - int(x[i])) == 1:
-            return True
-
-
-if n == 1:
-    print(9)
-else:
-    for i in range(10 ** (n - 1), 10 ** n):
-        if stair(i):
-            d[i] = d[i - 1] + 1
+for i in range(2, n + 1) :
+    for j in range(10):
+        if j == 0:
+            d[i][j] = d[i-1][1]
+        elif j == 9:
+            d[i][j] = d[i-1][8]
         else:
-            d[i] = d[i - 1]
-
-    print(d[(10 ** n) - 1] % 1000000000)
+            d[i][j] = d[i-1][j-1] + d[i-1][j+1]
+print(sum(d[n]) % 1000000000)
