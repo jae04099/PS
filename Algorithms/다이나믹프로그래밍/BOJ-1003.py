e1e06dba434a9e9d8724af46d73218775cee0f58
GyuriLee <jae04099@gmail.com>
2021-03-12 01:10:00 +0900

    boj-1003

---
+++ Algorithms/다이나믹프로그래밍/BOJ-1003.py
@@ -0,0 +1,14 @@
+t = int(input())
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+def dp(n):
+    length = len(zero)
+    if n >= length:
+        for i in range(length, n + 1):
+            zero.append(zero[i - 1] + zero[i - 2])
+            one.append(one[i - 1] + one[i + 2])
+    print("%d %d"%(zero[n], one[n]))
+for i in range(a):
+    k = int(input())
+    dp(k)
