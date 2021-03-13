6c0d720a0153254b47fff041b0b779440859f227
gyurilee <gyurilee.me@gmail.com>
2021-03-13 12:56:26 +0900

    boj-2156

--- Algorithms/다이나믹프로그래밍/BOJ-2156.py
+++ Algorithms/다이나믹프로그래밍/BOJ-2156.py
@@ -1,4 +1,11 @@
 n = int(input())
-d = [0]
+d = [0]*10001
+lists = []
 for i in range(n):
-    d.append(int(input()))
+    lists.append(int(input()))
+d[0] = lists[0]
+d[1] = lists[1]
+d[2] = lists[2]
+for i in range(3, n):
+    d[i] = max(d[i - 1] + d[i - 3], d[i - 2] + d[i - 3]) + d[i]
+print(d)
