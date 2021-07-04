3690dba38195c03046693b54ed737b12770dfa93
Gyuri Lee <jae04099@catholic.ac.kr>
2021-07-05 00:37:30 +0900

    boj1654 end

--- DataStructure/배열/BOJ-1654.py
+++ DataStructure/배열/BOJ-1654.py
@@ -1,16 +1,18 @@
 import sys
-K, N = map(int, input().split())
-lan = [int(sys.stdin.readline()) for _ in range(K)]
-start, end = 1, max(lan) #이분탐색 처음과 끝위치
+k, n = map(int,sys.stdin.readline().split())
+lines = []
+for i in range(k):
+    lines.append(int(input()))
+start = 1
+end = max(lines)
 
-while start <= end: #적절한 랜선의 길이를 찾는 알고리즘
-    mid = (start + end) // 2 #중간 위치
-    lines = 0 #랜선 수
-    for i in lan:
-        lines += i // mid #분할 된 랜선 수
-        
-    if lines >= N: #랜선의 개수가 분기점
-        start = mid + 1
+while start <= end:
+    middle = (end + start) // 2
+    line = 0
+    for i in lines:
+        line += i // middle
+    if line >= n:
+        start = middle + 1
     else:
-        end = mid - 1
+        end = middle - 1
 print(end)
