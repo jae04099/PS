db0f607194fe7d2cded7a9f9b97605097a03e849
Gyuri Lee <jae04099@catholic.ac.kr>
2021-09-15 20:18:36 +0900

    fin pro 모의고사

--- Algorithms/브루트포스/PGM-모의고사.py
+++ Algorithms/브루트포스/PGM-모의고사.py
@@ -18,4 +18,28 @@
 # print(finresult)
         
 
-        
+def solution(answers):
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    res = [0, 0, 0]
+
+    fin_res = []
+    n = 0
+    while n <= len(answers) - 1:
+        if answers[n] == first[n % (len(first))]:
+            res[0] += 1
+        if answers[n] == second[n % (len(second))]:
+            res[1] += 1
+        if answers[n] == third[n % (len(third))]:
+            res[2] += 1
+        n += 1
+
+
+    max_value = max(res)
+    for i in range(len(res)):
+        if res[i] == max_value:
+            fin_res.append(i + 1)
+    return fin_res
+print(solution([1, 3, 2, 4, 2]))
